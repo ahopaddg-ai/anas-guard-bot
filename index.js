38e9fa55a7fa49f619a20d5db7ffaaee5ecee864
abdalrhmano123 <ahopaddg@gmail.com>
2026-04-26 18:47:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+import { Client } from 'meowsab';
+import { group, access } from "./system/control.js";
+import UltraDB from "./system/UltraDB.js";
+import sub from './sub.js';
+
+/* =========== Client ========== */
+const client = new Client({
+  phoneNumber: '20123456789', // Bot number
+  prefix: [".", "/", "!"],
+  fromMe: false, 
+  owners: [
+  // Owner 1
+    { name: "ALHWARY", jid: "201556853817@s.whatsapp.net" }
+  ],
+  settings: { noWelcome: true },
+  commandsPath: './plugins'
+});
+
+client.onGroupEvent(group);
+client.onCommandAccess(access);
+
+/* =========== Database ========== */
+if (!global.db) {
+    global.db = new UltraDB();
+}
+
+/* =========== Config ========== */
+const { config } = client;
+config.info = { 
+  nameBot: "ALHWARY", 
+  nameChannel: "ALHWARY", 
+  idChannel: "120363225356834044@newsletter",
+  urls: {
+    repo: "https://github.com/deveni0/Pomni-AI",
+    api: "https://emam-api.web.id",
+    channel: "https://whatsapp.com/channel/0029VaQim2bAu3aPsRVaDq3v"
+  },
+  copyright: { 
+    pack: 'ALHWARY', 
+    author: 'ALHWARY'
+  },
+  images: [
+    "https://i.pinimg.com/originals/11/26/97/11269786cdb625c60213212aa66273a9.png",
+    "https://i.pinimg.com/originals/e2/21/20/e221203f319df949ee65585a657501a2.jpg",
+    "https://i.pinimg.com/originals/bb/77/0f/bb770fad66a634a6b3bf93e9c00bf4e5.jpg"
+  ]
+};
+
+/* =========== Start ========== */
+client.start();
+
+setTimeout(async () => {
+if (client.commandSystem) { 
+sub(client)
+  }
+}, 2000);
+
+
+/* =========== Catch Errors ========== */
+process.on('uncaughtException', (e) => {
+    if (e.message.includes('rate-overlimit')) {}
+});
+
+process.on('unhandledRejection', (err) => {
+    console.error('Unhandled Rejection:', err)
+});
